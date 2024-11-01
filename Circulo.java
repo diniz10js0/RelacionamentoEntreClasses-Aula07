36398da6efa25a0feea0126b411d2b051af61380
diniz10js0 <alvesdinizgustavo@gmail.com>
2024-10-31 21:20:07 -0300

    Add files via upload

---
+++ Circulo.java
@@ -0,0 +1,16 @@
+public class Circulo extends Forma
+{  private double raio;
+
+
+   public Circulo(double raio)
+   {  this.raio= raio;
+   }
+   
+   public double calcularArea()
+   { return Math.PI * raio * raio;
+   }
+   
+   public double calcularPerimetro()
+   { return 2* Math.PI * raio;
+   }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main
+{  public static void main(String[] args)
+   { Cachorro cachorro = new Cachorro ();
+     cachorro.mover(); // chamada para o método mover() da classe Animal   
+     cachorro.latir(); // chamada para o método latir() da classe Cachorro
+     }
+} 
